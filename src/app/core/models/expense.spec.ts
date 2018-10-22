83086cd2f18b8f3b48e8bc4c53f997dd688b1266
Szabolcs Hodossy <hodossy.szabolcs@gmail.com>
2018-10-22 16:09:24 +0200

    added custom constructors to expense

--- src/app/core/models/expense.spec.ts
+++ src/app/core/models/expense.spec.ts
@@ -7,8 +7,8 @@ describe('Expense', () => {
     expect(new Expense()).toBeTruthy();
     expect(new Expense(10)).toBeTruthy();
     expect(new Expense(10, 'HUF')).toBeTruthy();
-    expect(new Expense(10, 'HUF', Date.now())).toBeTruthy();
-    expect(new Expense(10, 'HUF', Date.now(), new Account())).toBeTruthy();
-    expect(new Expense(10, 'HUF', Date.now(), new Account(), new Category)).toBeTruthy();
+    expect(new Expense(10, 'HUF', new Date())).toBeTruthy();
+    expect(new Expense(10, 'HUF', new Date(), new Account())).toBeTruthy();
+    expect(new Expense(10, 'HUF', new Date(), new Account(), new Category)).toBeTruthy();
   });
 });
--- src/app/core/models/expense.ts
+++ src/app/core/models/expense.ts
@@ -7,4 +7,12 @@ export class Expense {
   public created: Date;
   public account: Account;
   public category: Category;
+
+  constructor(value?: number, currency?: string, created?: Date, account?: Account, category?: Category) {
+    this.value = value;
+    this.currency = currency;
+    this.created = created;
+    this.account = account;
+    this.category = category;
+  };
 }

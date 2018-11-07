3a831d9f820a878e019eef0c844a7d7d7f1a1650
Hodossy Szabolcs <szabolcs.hodossy@nokia.com>
2018-11-07 09:02:07 +0100

    Fixed JSON conversions for expense

--- src/app/core/models/expense.spec.ts
+++ src/app/core/models/expense.spec.ts
@@ -9,6 +9,42 @@ describe('Expense', () => {
     expect(new Expense(10, 'HUF')).toBeTruthy();
     expect(new Expense(10, 'HUF', new Date())).toBeTruthy();
     expect(new Expense(10, 'HUF', new Date(), new Account())).toBeTruthy();
-    expect(new Expense(10, 'HUF', new Date(), new Account(), new Category)).toBeTruthy();
+    expect(new Expense(10, 'HUF', new Date(), new Account(), new Category())).toBeTruthy();
+  });
+
+  it('should serialize created date as timestamp', () => {
+    let created = new Date();
+    let expense = new Expense(10, 'HUF', created);
+    expect(JSON.stringify(expense))
+      .toEqual(`{"value":10,"currency":"HUF","created":${created.getTime()},"account":null,"category":null}`)
+  });
+
+  it('should be revived without account or category', () => {
+    let created = new Date();
+    let expense = new Expense(10, 'HUF', created);
+    expect(JSON.parse(`{"value":10,"currency":"HUF","created":${created.getTime()},"account":null,"category":null}`, Expense.reviver))
+      .toEqual(expense);
+  });
+
+  it('should be revived with account and category', () => {
+    let created = new Date();
+    let expense = new Expense(10, 'HUF', created, new Account(1), new Category(1));
+    expect(JSON.parse(`{"value":10,"currency":"HUF","created":${created.getTime()},"account":1,"category":1}`, Expense.reviver))
+      .toEqual(expense);
+  });
+
+  it('should be converted to and from JSON equally', () => {
+    var expense = new Expense();
+    expect(Expense.fromJSON(JSON.stringify(expense))).toEqual(expense);
+    expense = new Expense(10);
+    expect(Expense.fromJSON(JSON.stringify(expense))).toEqual(expense);
+    expense = new Expense(10, 'HUF');
+    expect(Expense.fromJSON(JSON.stringify(expense))).toEqual(expense);
+    expense = new Expense(10, 'HUF', new Date());
+    expect(Expense.fromJSON(JSON.stringify(expense))).toEqual(expense);
+    expense = new Expense(10, 'HUF', new Date(), new Account(1));
+    expect(Expense.fromJSON(JSON.stringify(expense))).toEqual(expense);
+    expense = new Expense(10, 'HUF', new Date(), new Account(1), new Category(1));
+    expect(Expense.fromJSON(JSON.stringify(expense))).toEqual(expense);
   });
 });
--- src/app/core/models/expense.ts
+++ src/app/core/models/expense.ts
@@ -1,6 +1,14 @@
 import { Account } from './account';
 import { Category } from './category';
 
+interface ExpenseJSON {
+  value: number;
+  currency: string;
+  created: number;
+  account: number;
+  category: number;
+}
+
 export class Expense {
   public value: number;
   public currency: string;
@@ -11,16 +19,33 @@ export class Expense {
   constructor(value?: number, currency?: string, created?: Date, account?: Account, category?: Category) {
     this.value = value;
     this.currency = currency;
-    this.created = created;
+    this.created = created || new Date();
     this.account = account;
     this.category = category;
   };
 
-  toJSON() {
-    return {value: this.value, currency: this.currency, created: this.created.getTime(), account: this.account.id, category: this.category.id}
+  toJSON(): ExpenseJSON {
+    return Object.assign({}, this, {
+      created: this.created.getTime(),
+      account: this.account ? this.account.id: null,
+      category: this.category ? this.category.id : null
+    });
+  }
+
+  static fromJSON(json: string|ExpenseJSON): Expense {
+    if (typeof json === 'string') {
+      return JSON.parse(json, Expense.reviver);
+    } else {
+      let expense = new Expense();
+      return Object.assign(expense, json, {
+        created: new Date(json.created),
+        account: json.account ? new Account(json.account) : undefined,
+        category: json.category ? new Category(json.category) : undefined,
+      });
+    }
   }
 
   static reviver(key: string, value: any): any {
-    return key === "created" ? new Date(value) : value;
+    return key === "" ? Expense.fromJSON(value) : value;
   }
 }

732607db1dc77041719c5b0df9a583563c6d4e8c
Hodossy Szabolcs <szabolcs.hodossy@nokia.com>
2018-11-10 14:25:40 +0100

    Added details to ExpenseDetailComponent

--- src/app/core/components/expense-detail/expense-detail.component.spec.ts
+++ src/app/core/components/expense-detail/expense-detail.component.spec.ts
@@ -1,14 +1,21 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { ExpenseDetailComponent } from './expense-detail.component';
+import { Expense } from '../../models';
+
+import { IndicatorDirective } from '../../directives';
 
 describe('ExpenseDetailComponent', () => {
   let component: ExpenseDetailComponent;
   let fixture: ComponentFixture<ExpenseDetailComponent>;
+  let compiled: any;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ ExpenseDetailComponent ]
+      declarations: [
+        ExpenseDetailComponent,
+        IndicatorDirective
+      ]
     })
     .compileComponents();
   }));
@@ -16,10 +23,33 @@ describe('ExpenseDetailComponent', () => {
   beforeEach(() => {
     fixture = TestBed.createComponent(ExpenseDetailComponent);
     component = fixture.componentInstance;
+    compiled = fixture.debugElement.nativeElement;
     fixture.detectChanges();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should show text when no expense is defined', () => {
+    expect(compiled.querySelector('span').textContent).toContain('No expense to show!');
+  });
+
+  it('should show value with currency', () => {
+    component.expense = new Expense(100, 'HUF');
+    fixture.detectChanges();
+    expect(compiled.querySelector('span').textContent).toContain('Ft100.00');
+    component.expense = new Expense(100, 'USD');
+    fixture.detectChanges();
+    expect(compiled.querySelector('span').textContent).toContain('$100.00');
+  });
+
+  it('should discriminate expenses from incomes', () => {
+    component.expense = new Expense(100, 'USD');
+    fixture.detectChanges();
+    expect(compiled.querySelector('span').classList).toContain('em-income');
+    component.expense = new Expense(-100, 'USD');
+    fixture.detectChanges();
+    expect(compiled.querySelector('span').classList).toContain('em-expense');
   });
 });
---
+++ src/app/core/directives/index.ts
@@ -0,0 +1,1 @@
+export { IndicatorDirective } from './indicator.directive';

5f993b1a43bea5e6dd252af0fdd2909b6addd3ea
Hodossy Szabolcs <szabolcs.hodossy@nokia.com>
2018-11-10 10:45:42 +0100

    Added planned components and core module skeletons

---
+++ src/app/core/core.module.ts
@@ -0,0 +1,24 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { AccountDetailComponent } from './components/account-detail/account-detail.component';
+import { AccountListComponent } from './components/account-list/account-list.component';
+import { CategoryDetailComponent } from './components/category-detail/category-detail.component';
+import { CategoryListComponent } from './components/category-list/category-list.component';
+import { ExpenseListComponent } from './components/expense-list/expense-list.component';
+import { ExpenseDetailComponent } from './components/expense-detail/expense-detail.component';
+
+@NgModule({
+  imports: [
+    CommonModule
+  ],
+  declarations: [
+    AccountDetailComponent,
+    AccountListComponent,
+    CategoryDetailComponent,
+    CategoryListComponent,
+    ExpenseListComponent,
+    ExpenseDetailComponent
+  ]
+})
+export class CoreModule { }

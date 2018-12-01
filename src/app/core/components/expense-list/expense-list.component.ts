ad5e073dda47a2db56eb79e430e8bfa73593fdb6
Hodossy Szabolcs <szabolcs.hodossy@nokia.com>
2018-12-01 12:31:58 +0100

    Added a basic material table for expense list

--- src/app/core/components/expense-list/expense-list.component.ts
+++ src/app/core/components/expense-list/expense-list.component.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { MatTableModule } from '@angular/material/table';
+
+import { Expense } from '../../models';
 
 @Component({
   selector: 'app-expense-list',
@@ -6,10 +9,31 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./expense-list.component.scss']
 })
 export class ExpenseListComponent implements OnInit {
+  @Input() expenses: Array<Expense>;
+  private displayedColumns = ['created', ]
+  _showAccount: boolean;
+  _showCategory: boolean;
+
+  @Input('noAccount')
+  set showAccount(value: boolean) {
+    this._showAccount = !value;
+  }
+
+  @Input('noCategory')
+  set showCategory(value: boolean) {
+    this._showCategory = !value;
+  }
 
   constructor() { }
 
   ngOnInit() {
+    if(this._showAccount) {
+      this.displayedColumns.push('account');
+    }
+    if(this._showCategory) {
+      this.displayedColumns.push('category');
+    }
+    this.displayedColumns.push('value');
   }
 
 }

6f480737019a9df7e0cbcd1bfd73e85ee95b7d83
Hodossy Szabolcs <szabolcs.hodossy@nokia.com>
2019-05-24 14:03:22 +0200

    Expenses are now appearing when added

--- src/app/components/account-page/account-page.component.ts
+++ src/app/components/account-page/account-page.component.ts
@@ -3,8 +3,8 @@ import { ActivatedRoute, ParamMap } from '@angular/router';
 import { Observable } from 'rxjs';
 import { switchMap } from 'rxjs/operators';
 
-import { Account } from '../../core/models';
-import { AccountService } from '../../core/services';
+import { Account, Expense } from '../../core/models';
+import { AccountService, ExpenseService } from '../../core/services';
 
 @Component({
   selector: 'em-account-page',
@@ -13,16 +13,23 @@ import { AccountService } from '../../core/services';
 })
 export class AccountPageComponent implements OnInit {
   account$: Observable<Account>;
+  expenses$: Observable<Expense[]>;
 
   constructor(
-    private service: AccountService,
+    private accountService: AccountService,
+    private expenseService: ExpenseService,
     private route: ActivatedRoute
   ) {}
 
   ngOnInit() {
     this.account$ = this.route.paramMap.pipe(
-      switchMap((params: ParamMap) => this.service.getById(params.get('id')))
+      switchMap((params: ParamMap) => this.accountService.getById(params.get('id')))
     );
+    this.expenses$ = this.account$.pipe(
+      switchMap((account: Account): Observable<Expense[]> => {
+        return this.expenseService.getExpensesForAccount$(account);
+      })
+    )
   }
 
 }
